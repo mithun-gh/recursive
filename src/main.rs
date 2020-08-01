3260c44bdfe9dbd235d5048dda862e2be78cafc6
Mithun <mithunrathinasamy@live.com>
2020-08-01 17:50:28 +1000

    feat: add model tce

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use recursive::tail_recursive;
 
-fn sum(n: u64) -> u64 {
+fn _sum(n: u64) -> u64 {
 
     #[tail_recursive]
     fn sum_rec(n: u64, a: u64) -> u64 {
@@ -13,6 +13,30 @@ fn sum(n: u64) -> u64 {
     sum_rec(n, 0)
 }
 
+enum Action<C, R> {
+    Continue(C),
+    Return(R),
+}
+
+fn sum_tc((n, a): (u64, u64)) -> Action<(u64, u64), u64> {
+    match n {
+        0 => return Action::Return(a),
+        _ => return Action::Continue((n - 1, n + a)),
+    };
+}
+
+fn executor(n: u64) -> u64 {
+    let mut acc = (n, 0);
+    loop {
+        match sum_tc(acc) {
+            Action::Return(_) => break,
+            Action::Continue(v) => acc = v,
+        }
+    }
+    acc.1
+}
+
 fn main() {
-    println!("Result: {}", sum(999_999));
+    // println!("Result: {}", sum(999_999));
+    println!("Result: {}", executor(999_999));
 }
