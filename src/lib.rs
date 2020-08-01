4f3d1c15516b7db1fca138b43a9c39538a560e24
Mithun <mithunrathinasamy@live.com>
2020-08-01 15:57:56 +1000

    feat: basic fn renamer

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,22 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{self, Ident, ItemFn, visit_mut::VisitMut};
+
+struct FnRenamer(String);
+
+impl VisitMut for FnRenamer {
+    fn visit_ident_mut(&mut self, ident: &mut Ident) {
+        if ident == self.0.as_str() {
+            *ident = Ident::new(format!("__internal_{}_rec", self.0).as_str(), ident.span());
+        }
+    }
+}
 
 #[proc_macro_attribute]
 pub fn recursive(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    item
+    let mut item: ItemFn = syn::parse(item).expect("parser error");
+
+    FnRenamer(item.sig.ident.to_string()).visit_item_fn_mut(&mut item);
+
+    (quote! { #item }).into()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use recursive::recursive;
 
 #[recursive]
-fn sum(n: i32) -> i32 {
+fn test(n: u64) -> u64 {
     match n {
         0 => 0,
-        _ => n + sum(n - 1),
+        _ => n + test(n - 1),
     }
 }
 
 fn main() {
-    println!("Sum: {}", sum(10));
+    println!("Result: {}", __internal_test_rec(999));
 }
