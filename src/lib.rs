3b34bbee6c15bfdac6b4552104ef5f271707c997
Mithun <mithunrathinasamy@live.com>
2020-08-02 15:35:58 +1000

    important update

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,40 @@
-use proc_macro::TokenStream;
+use proc_macro::{TokenStream, Span};
 use quote::quote;
-use syn::{self, ItemFn, ExprReturn, visit_mut::VisitMut};
+use syn::*;
+use syn::visit_mut::VisitMut;
+
+#[allow(unused)]
+#[derive(Debug)]
+enum Action<C, R> {
+    Continue(C),
+    Return(R),
+}
+
+macro_rules! make {
+    ($make_type:ty, $($tokens:tt)+) => {
+        parse::<$make_type>(TokenStream::from(quote! { $($tokens)+ })).unwrap()
+    };
+}
 
 struct FnVisitor;
 
 impl VisitMut for FnVisitor {
-    fn visit_expr_return_mut(&mut self, expr_return: &mut ExprReturn) {
-        println!("{:?}\n", expr_return.expr)
+    fn visit_return_type_mut(&mut self, return_ty: &mut ReturnType) {
+        let t = make!(ReturnType, -> String);
+        println!(">> {:?}", t);
+        *return_ty = t;
     }
 }
 
 #[proc_macro_attribute]
-pub fn tail_recursive(_attr: TokenStream, item: TokenStream) -> TokenStream {
+pub fn recursive(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let mut item: ItemFn = syn::parse(item).unwrap();
 
     FnVisitor.visit_item_fn_mut(&mut item);
 
-    (quote! { #item }).into()
+    (quote!{ #item }).into()
+}
+
+fn _make_ident(name: &str) -> Ident {
+    Ident::new(name, Span::call_site().into())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,24 @@
-use recursive::tail_recursive;
+use recursive::recursive;
 
-fn _sum(n: u64) -> u64 {
-
-    #[tail_recursive]
-    fn sum_rec(n: u64, a: u64) -> u64 {
-        match n {
-            0 => return a,
-            _ => return sum_rec(n - 1, n + a),
-        };
-    }
-
-    sum_rec(n, 0)
-}
-
-enum Action<C, R> {
-    Continue(C),
-    Return(R),
-}
-
-fn sum_tc((n, a): (u64, u64)) -> Action<(u64, u64), u64> {
+#[recursive]
+fn sum(n: u64, a: u64) -> u64 {
     match n {
-        0 => return Action::Return(a),
-        _ => return Action::Continue((n - 1, n + a)),
-    };
+        0 => return a,
+        _ => return sum(n - 1, n + a),
+    }
 }
 
-fn executor(n: u64) -> u64 {
-    let mut acc = (n, 0);
-    loop {
-        match sum_tc(acc) {
-            Action::Return(_) => break,
-            Action::Continue(v) => acc = v,
-        }
+/*
+
+fn sum(n: u64, a: u64) -> u64 {
+    match n {
+        0 => return a,
+        _ => return sum(n - 1, n + a),
     }
-    acc.1
 }
+
+*/
 
 fn main() {
-    // println!("Result: {}", sum(999_999));
-    println!("Result: {}", executor(999_999));
+    println!("Result: {}", sum(10, 0));
 }
