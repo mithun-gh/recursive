c50f26b9205bf6c82835f6dc657b14bb976a266e
Mithun <mithunrathinasamy@live.com>
2020-08-10 22:37:29 +1000

    add one more example

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn sum(n: u64, a: u64) -> u64 {
         _ => sum(n - 1, n + a),
     }
 }
- 
+
 #[recursive]
 fn factorial(n: u64, a: u64) -> u64 {
     if n == 0 {
@@ -17,7 +17,17 @@ fn factorial(n: u64, a: u64) -> u64 {
     }
 }
 
+#[recursive]
+fn repeat(input: &str, n: usize, a: String) -> String {
+    if n == 0 {
+        a
+    } else {
+        repeat(input, n - 1, format!("{}{}", input, a))
+    }
+}
+
 fn main() {
     println!("Result: {}", sum(999_999, 0));
     println!("Result: {}", factorial(10, 1));
+    println!("{}", repeat("*", 10, String::new()));
 }
